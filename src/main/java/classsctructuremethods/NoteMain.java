21c1bf4dfb0683f92c54209de1663cef9cc48714
Kondi <emmanuel>
2020-10-29 21:25:00 +0100

    Jegyzeteljünk

---
+++ src/main/java/classsctructuremethods/NoteMain.java
@@ -0,0 +1,20 @@
+package classsctructuremethods;
+
+import java.util.Scanner;
+
+public class NoteMain {
+    public static void main(String[] args) {
+        Note jegyzet = new Note();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name: ");
+        jegyzet.setName(scanner.nextLine());
+        System.out.println("Topic: ");
+        jegyzet.setTopic(scanner.nextLine());
+        System.out.println("Text: ");
+        jegyzet.setText(scanner.nextLine());
+
+        System.out.println("\n"+jegyzet.getNoteText());
+    }
+}
