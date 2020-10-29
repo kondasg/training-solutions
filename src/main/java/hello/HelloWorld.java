23e991bc6a9972f698e9e2a8e182153fea3382a2
Kondi <emmanuel>
2020-10-29 15:43:34 +0100

    változó használata

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,6 +2,7 @@ package hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String message = "Hello World!";
+        System.out.println(message);
     }
 }
