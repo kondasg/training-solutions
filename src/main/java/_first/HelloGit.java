9684d5e41b1326c96fc1abe4601da5077566cd0b
Kondi <emmanuel>
2020-10-29 01:10:58 +0100

    clean

--- src/main/java/_first/HelloGit.java
+++
@@ -1,7 +0,0 @@
-package _first;
-
-public class HelloGit {
-    public static void main(String[] args) {
-        System.out.println("Hello GitHub!");
-    }
-}
--- src/main/java/_sec/_sec.java
+++
@@ -1,7 +0,0 @@
-package _sec;
-
-public class _sec {
-    public static void main(String[] args) {
-        System.out.println("Hello GitHub2!");
-    }
-}
