a1c8e9a36028cbb43c868fcd4308ac3c4d9b5d55
Kondi <emmanuel>
2020-11-05 10:27:08 +0100

    órai munka

---
+++ src/main/java/week02/InputNames.java
@@ -0,0 +1,20 @@
+package week02;
+
+import java.util.Scanner;
+
+public class InputNames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] names = new String[5];
+
+        for (int i=1; i<=5; i++) {
+            System.out.println("Kérem az " + i + ". nevet: ");
+            names[i-1] = scanner.nextLine();
+        }
+
+        for (String name: names) {
+            System.out.println(name);
+        }
+    }
+}
