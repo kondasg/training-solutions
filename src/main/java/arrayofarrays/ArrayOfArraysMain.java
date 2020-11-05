267e9d240f25f01cd03df0859863e9c9edf78d72
Kondi <emmanuel>
2020-11-05 22:02:19 +0100

    Háromszögmátrix

--- src/main/java/arrayofarrays/ArrayOfArraysMain.java
+++ src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -7,7 +7,7 @@ public class ArrayOfArraysMain {
 
         for (int i = 1; i <= size; i++) {
             for (int j = 1; j <= size; j++) {
-                m[i - 1][j - 1] = i*j;
+                m[i - 1][j - 1] = i * j;
             }
         }
         return m;
@@ -22,10 +22,23 @@ public class ArrayOfArraysMain {
         }
     }
 
+    public int[][] triangularMatrix(int size) {
+        int[][] t = new int[size][];
+
+        for (int i = 0; i < size; i++) {
+            t[i] = new int[i + 1];
+            for (int j = 0; j < i + 1; j++) {
+                t[i][j] = i;
+            }
+        }
+        return t;
+    }
 
     public static void main(String[] args) {
         ArrayOfArraysMain arrayOfArraysMain = new ArrayOfArraysMain();
 
         arrayOfArraysMain.printArrayOfArrays(arrayOfArraysMain.multiplicationTable(4));
+        System.out.println();
+        arrayOfArraysMain.printArrayOfArrays(arrayOfArraysMain.triangularMatrix(4));
     }
 }
