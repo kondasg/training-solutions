aedd16506bc312a6f4351388aefee5b98f62c40c
Kondi <emmanuel>
2020-11-05 12:52:01 +0100

    - final

--- src/main/java/week02/Product.java
+++ src/main/java/week02/Product.java
@@ -26,8 +26,6 @@ public class Product {
         Product product2 = new Product("alma", "1234");
         Product product3 = new Product("alma2", "123");
 
-
-
         System.out.println("product1 és product2 egyenlő? "+ product1.areTheyEqual(product2));
         System.out.println("product1 és product3 egyenlő? "+ product1.areTheyEqual(product3));
         System.out.println("product2 és product3 egyenlő? "+ product2.areTheyEqual(product3));
