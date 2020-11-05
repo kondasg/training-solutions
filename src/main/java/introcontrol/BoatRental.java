ee547cd63646838908a45dbf12e0a8fdda38dc17
Kondi <emmanuel>
2020-11-05 17:11:15 +0100

    Csónakok

---
+++ src/main/java/introcontrol/BoatRental.java
@@ -0,0 +1,18 @@
+package introcontrol;
+
+import java.util.Scanner;
+
+public class BoatRental {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int csonak1 = 5;
+        int csonak2 = 3;
+        int csonak3 = 2;
+
+        System.out.print("Csoport létszáma: ");
+        scanner.nextInt();
+
+//erre még visszatérek
+
+    }
+}
