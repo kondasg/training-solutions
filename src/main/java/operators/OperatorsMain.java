aa564ce028f3cf79f44ab8ff06053a50ca7c0c41
Kondi <emmanuel>
2020-11-10 23:06:46 +0100

    Bónusz feladatOK

--- src/main/java/operators/OperatorsMain.java
+++ src/main/java/operators/OperatorsMain.java
@@ -7,5 +7,22 @@ public class OperatorsMain {
         System.out.println(operators.isEven(2));
 
         System.out.println(operators.multiplyByPowerOfTwo(5, 5));
+
+        System.out.println();
+
+        //System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
+        int i = -1;
+        String s = Integer.toBinaryString(i);
+        System.out.println(s);
+        //int j = Integer.parseInt(s, 2);
+        int j = Long.valueOf(s, 2).intValue();
+        System.out.println(j);
+        // Miért ad a következő kódrészlet kivételt? s értéke nagyobb mint az Integer tartomány (számábrázolás 32bit)
+        // Miért megoldás erre a Long.valueOf(s, 2).intValue() kifejezés használata? Mert nagyonn a Long értelmezési tartománya (számábrázolás  64bit)
+
+        System.out.println();
+
+        System.out.println(0333);
+        // oktális 333 az decimális 219
     }
 }
