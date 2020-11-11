4f910a0de5df498c085509ed0dd77c6ecc5ec700
Kondi <emmanuel>
2020-11-11 09:46:39 +0100

    Első teszt implementálása

---
+++ src/test/java/introjunit/GentlemanTest.java
@@ -0,0 +1,14 @@
+package introjunit;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class GentlemanTest {
+    @Test
+    public void testSayHello() {
+        Gentleman gentleman = new Gentleman();
+        assertThat(gentleman.sayHello("John Doe"), equalTo("Hello John Doe"));
+    }
+}
