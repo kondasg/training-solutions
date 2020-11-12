0c6eb9d2411b484b59f47afe5618c877b25bdc58
Kondi <emmanuel>
2020-11-12 22:38:39 +0100

    Hónap napjainak visszaadása

---
+++ src/main/java/controlselection/month/DayInMonth.java
@@ -0,0 +1,27 @@
+package controlselection.month;
+
+public class DayInMonth {
+    public int numberOfDays(int year, String month) {
+        switch (month.toLowerCase()) {
+            case "január":
+            case "március":
+            case "május":
+            case "július":
+            case "augusztus":
+            case "október":
+            case "december":
+                return 31;
+            case "":
+            case "április":
+            case "június":
+            case "szeptembber":
+            case "november":
+                return 30;
+            case "február":
+                if (((year % 4 == 0) && !(year % 100 == 0)) || (year % 400 == 0) ) return 29;
+                else return 28;
+            default:
+                throw new IllegalArgumentException("Invalid month: " + month);
+        }
+    }
+}
