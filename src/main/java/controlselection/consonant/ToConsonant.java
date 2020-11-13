ab856f60a2f0d6ca33a40573ef5d9327a5cca413
Kondi <emmanuel>
2020-11-13 09:57:26 +0100

    kiegészítés a pattern-be: nagy betűk

--- src/main/java/controlselection/consonant/ToConsonant.java
+++ src/main/java/controlselection/consonant/ToConsonant.java
@@ -2,7 +2,7 @@ package controlselection.consonant;
 
 public class ToConsonant {
     public char convertToConsonant(char c) {
-        String pattern = "aeiou";
+        String pattern = "aeiouAEIOU";
         if (pattern.indexOf(c) >= 0) return (char) (c + 1);
         else return c;
     }
