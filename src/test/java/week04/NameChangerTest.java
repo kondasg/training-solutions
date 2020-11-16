999c79ac736ea80be61ba73e267a3f44c1509b84
Kondi <emmanuel>
2020-11-16 16:03:06 +0100

    teszt Exception

--- src/test/java/week04/NameChangerTest.java
+++ src/test/java/week04/NameChangerTest.java
@@ -3,11 +3,18 @@ package week04;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class NameChangerTest {
 
     @Test
-    void changeFirstName() {
+    void createNameChangerTest() {
+        IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, ()->  new NameChanger(""));
+        assertEquals("Invalid Name:", iae.getMessage());
+    }
+
+    @Test
+    void changeFirstNameTest() {
         NameChanger nameChanger = new NameChanger("Kiss Józsi");
         nameChanger.changeFirstName("Béla");
         assertEquals("Kiss Béla", nameChanger.getFullName() );
