0b963b4ea947de0873b24e94f4ccec4d92894ff7
Kondi <emmanuel>
2020-11-17 23:41:30 +0100

    Összes jóváírás

---
+++ src/main/java/algorithmssum/transactions/TransactionSumCalculator.java
@@ -0,0 +1,14 @@
+package algorithmssum.transactions;
+
+import java.util.List;
+
+public class TransactionSumCalculator {
+
+    public int sumAmountOfCreditEntries(List<Transaction> transactions) {
+        int sum = 0;
+        for (Transaction item: transactions) {
+            if (item.isCredit()) sum += item.getAmount();
+        }
+        return sum;
+    }
+}
