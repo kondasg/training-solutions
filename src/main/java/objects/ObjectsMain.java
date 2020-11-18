8197196e008a39a118c949eebda6ebd6338a7c3c
Kondi <emmanuel>
2020-11-18 10:48:40 +0100

    jav

---
+++ src/main/java/objects/ObjectsMain.java
@@ -0,0 +1,18 @@
+package objects;
+
+public class ObjectsMain {
+    public static void main(String[] args) {
+        Book book = new Book();
+
+        System.out.println(book);
+
+        Book emptyBook = null;
+        System.out.println(emptyBook);
+
+        Boolean x;
+
+
+
+
+    }
+}
