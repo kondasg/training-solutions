b4eb227a69927282bf894ca9304e0cceeae92f66
Kondi <emmanuel>
2020-11-18 23:58:50 +0100

    String-ek

---
+++ src/main/java/stringbasic/stringbasic/StringCreator.java
@@ -0,0 +1,11 @@
+package stringbasic.stringbasic;
+
+public class StringCreator {
+    public String createStringForHeap() {
+        return "abc";
+    }
+
+    public String createStringForPool() {
+        return "abc";
+    }
+}
