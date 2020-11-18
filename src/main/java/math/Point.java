2883acf0b9b5b66c214eadd6bca0c353780e30fc
Kondi <emmanuel>
2020-11-18 11:10:12 +0100

    parseInt

---
+++ src/main/java/math/Point.java
@@ -0,0 +1,22 @@
+package math;
+
+public class Point {
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public double distance(Point other) {
+        return 0.0;
+    }
+}
