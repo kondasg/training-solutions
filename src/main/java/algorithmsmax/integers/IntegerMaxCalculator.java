2e3e3afeb466ac6984a2f6a4d73eded03e1e8971
Kondi <emmanuel>
2020-11-18 01:03:21 +0100

    MIN_VALUE

--- src/main/java/algorithmsmax/integers/IntegerMaxCalculator.java
+++ src/main/java/algorithmsmax/integers/IntegerMaxCalculator.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class IntegerMaxCalculator {
 
     public int max(List<Integer> nums) {
-        int max = 0;
+        int max = Integer.MIN_VALUE;
         for (int num: nums) {
             if ( max < num ) max = num;
         }
