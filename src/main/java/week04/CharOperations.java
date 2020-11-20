457028bc41fe60e23cfc16ad1e581b69d78ce74e
Kondi <emmanuel>
2020-11-20 10:32:27 +0100

    órai feladat

---
+++ src/main/java/week04/CharOperations.java
@@ -0,0 +1,22 @@
+package week04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CharOperations {
+
+    public List<Integer> getPositionOfChar(String base, String part) {
+        List<Integer> poz = new ArrayList<>();
+        for (int i = 0; i < base.length(); i++) {
+            if ("a".equals(base.substring(i, i + 1))) {
+                poz.add(i);
+            }
+        }
+        return poz;
+    }
+
+    public static void main(String[] args) {
+        CharOperations c = new CharOperations();
+        System.out.println(c.getPositionOfChar("alma", "a"));
+    }
+}
---
+++ src/test/java/week04/CharOperationsTest.java
@@ -0,0 +1,17 @@
+package week04;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CharOperationsTest {
+    @Test
+    void Test() {
+        CharOperations c = new CharOperations();
+        List<Integer> x = c.getPositionOfChar("alma", "a");
+        assertEquals(2, x.size());
+    }
+}
