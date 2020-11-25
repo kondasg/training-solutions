7f917d9d46b223d60178fa3536a19f155078527d
kondasg <gabor@kondas.hu>
2020-11-25 10:55:56 +0100

    Ádám megoldása

--- src/main/java/week05d03/UserValidator.java
+++ src/main/java/week05d03/UserValidator.java
@@ -4,10 +4,19 @@ import java.util.List;
 
 public class UserValidator {
 
+//    public void validate(List<User> users) {
+//        for (User user: users) {
+//            if ("".equals(user.getName())) throw new IllegalArgumentException("Üres String!");
+//            if ( user.getAge() < 0 || user.getAge() > 120 ) throw new IllegalArgumentException("nem megfelő age!");
+//        }
+//    }
+
     public void validate(List<User> users) {
+        if (users == null) throw new IllegalArgumentException("Users is null!");
         for (User user: users) {
-            if ("".equals(user.getName())) throw new IllegalArgumentException("Üres String!");
-            if ( user.getAge() < 0 || user.getAge() > 120 ) throw new IllegalArgumentException("nem megfelő age!");
+            if (user == null) throw new IllegalArgumentException("User is null!");
+            if (user.getName() ==  null || user.getName().isBlank()) throw new IllegalArgumentException("The user name can't be blank or null");
+            if (user.getAge() < 0 || user.getAge() > 120) throw new IllegalArgumentException("The user age must be between 0 and 120!");
         }
     }
 }
