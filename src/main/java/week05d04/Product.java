aa11e1faa90d30f0692dfb4a2e94474398059095
kondasg <gabor@kondas.hu>
2020-11-26 12:41:43 +0100

    +feladatkiírás

--- src/main/java/week05d04/Product.java
+++ src/main/java/week05d04/Product.java
@@ -20,5 +20,17 @@ public class Product {
         return price / Currency.valueOf(currency).getRate();
     }
 
+/*
+Készíts egy osztályt Product néven, melynek van egy price (long) és egy currency (String) mezője.
+A Product a konstruktorában ellenőrizze a currency mezőt, melynek az értéke vagy HUF, vagy USD lehet.
+Készíts egy metódust convertPrice néven, melynek egy paramétere van (currency) és visszaadja a currency-nek megfelelő összeget (double).
+Használjunk 300 forintos átváltási árfolyamot!
+
+Példa: ha a Product-ot létrehozzuk a következőképp: new Product(150, "HUF"),
+majd meghívjuk a convertPrice-ot "USD" paraméterrel, akkor az elvárt visszatérési érték 0.5.
+
+Bónusz feladat: írjunk teszteket
+Bónusz feladat 2: Használjunk enum-ot String helyett a pénznem reprezentációjára
+ */
 
 }
