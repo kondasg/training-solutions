929b3b719af2512873ddc1fd3eb88ed816c79a7c
kondasg <gabor@kondas.hu>
2020-12-06 10:08:27 +0100

    Space agency

---
+++ src/main/java/immutable/satellite/SpaceAgency.java
@@ -0,0 +1,29 @@
+package immutable.satellite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpaceAgency {
+
+    private final List<Satellite> satellites = new ArrayList<>();
+
+    public void registerSatellite(Satellite satellite) {
+        if (satellite == null) {
+            throw new NullPointerException("Parameter must not be null!");
+        }
+        satellites.add(satellite);
+    }
+
+    public Satellite findSatelliteByRegisterIdent(String registerIdent) {
+        for (Satellite satellite: satellites) {
+            if (satellite.getRegisterIdent().equals(registerIdent)) {
+                return satellite;
+            }
+        }
+        throw new IllegalArgumentException("Satellite with the given registration cannot be found!" + registerIdent);
+    }
+
+    public String toString() {
+        return satellites.toString();
+    }
+}
