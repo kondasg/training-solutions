1f0942cfd2f0d0ceedf02404e58ba1ccbf86386d
kondasg <gabor@kondas.hu>
2020-12-07 15:43:35 +0100

    Bónusz feladat - Kocsmatúra

---
+++ src/main/java/methodoverloading/pub/ListOfGoodPubs.java
@@ -0,0 +1,25 @@
+package methodoverloading.pub;
+
+import java.util.List;
+
+public class ListOfGoodPubs {
+
+    private final List<Pub> goodPubs;
+
+    public ListOfGoodPubs(List<Pub> goodPubs) {
+        if (goodPubs == null || goodPubs.size() == 0) {
+            throw new IllegalArgumentException("Pub list is empty!");
+        }
+        this.goodPubs = goodPubs;
+    }
+
+    public Pub findTheBest() {
+        Pub best = goodPubs.get(0);
+        for (Pub pub: goodPubs) {
+            if (pub.getOpenFrom().isEarlier(best.getOpenFrom()) ) {
+                best = pub;
+            }
+        }
+        return best;
+    }
+}
---
+++ src/main/java/methodoverloading/pub/Pub.java
@@ -0,0 +1,26 @@
+package methodoverloading.pub;
+
+import methodoverloading.Time;
+
+public class Pub {
+
+    private final String name;
+    private final Time openFrom;
+
+    public Pub(String name, int hours, int minutes) {
+        this.name = name;
+        this.openFrom = new Time(hours, minutes);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Time getOpenFrom() {
+        return openFrom;
+    }
+
+    public String toString() {
+        return name + ";" + openFrom.getHours() + ":" + openFrom.getMinutes();
+    }
+}
