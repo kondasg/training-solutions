d07475df6f83e96720130fdced397e7b66ff5c5a
kondasg <gabor@kondas.hu>
2020-12-10 10:15:20 +0100

    +test

---
+++ src/test/java/week07d04/calculateSumTest.java
@@ -0,0 +1,13 @@
+package week07d04;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class calculateSumTest {
+
+    @Test
+    void Test() {
+        assertEquals(420, new ShoppingList().calculateSum("list.txt"));
+    }
+}
