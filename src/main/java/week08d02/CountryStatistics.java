438b991ed3ccc0f08f4785a18a63d2a771158494
kondasg <gabor@kondas.hu>
2020-12-15 10:35:37 +0100

    maxPopulaton() ésszerűsítés

--- src/main/java/week08d02/CountryStatistics.java
+++ src/main/java/week08d02/CountryStatistics.java
@@ -21,7 +21,6 @@ public class CountryStatistics {
             String line;
             while((line = reader.readLine()) != null) {
                 String[] col = line.split(" ");
-
                 countries.add(new Country(col[0], Integer.parseInt(col[1]), Integer.parseInt(col[2]), Integer.parseInt(col[3])));
             }
         }
@@ -31,15 +30,13 @@ public class CountryStatistics {
     }
 
     public Country maxPopulaton() {
-        int maxPopulation = 0;
-        int index = 0;
-        for (int i = 0; i < countries.size(); i++) {
-            if (maxPopulation < countries.get(i).getPopulation()) {
-                maxPopulation = countries.get(i).getPopulation();
-                index = i;
+        Country maxPopulation = countries.get(0);
+        for (Country country: countries) {
+            if (maxPopulation.getPopulation() < country.getPopulation()) {
+                maxPopulation = country;
             }
         }
-        return countries.get(index);
+        return maxPopulation;
     }
 
     public static void main(String[] args) {
@@ -50,3 +47,24 @@ public class CountryStatistics {
 
 
 }
+
+// Adott a countries.txt állomány, melyet helyezz el a projekt gyökérkönyvtárába.
+// A fájlban az adatok a következők: ország_neve népesség hány_színű_a_zászló szomszédos_országok_száma.
+// Készíts a week08d02 csomagba egy CountryStatistics osztályt melyben van egy metódus,
+// ami az adatokat beolvassa egy listába.
+// A lista legyen az osztály privát attribútuma, és legyen hozzá getter metódus,
+// figyelj arra, hogy ezen keresztül ne lehessen megváltoztatni a listát.
+// Készíts egy metódust maxPopulaton() néven, ami a listából visszaadja a legnagyobb népességű ország minden adatát.
+
+// countries.txt
+//        Nepal 31 3 2
+//        Csad 13 3 6
+//        Kongoi_Demokratikus_Koztarsasag 79 3 9
+//        Honduras 8 2 3
+//        Kirgizisztán 6 2 4
+//        Dzsibuti 1 4 3
+//        Togo 7 4 3
+//        Kambodzsa 15 3 3
+//        Guyana 1 5 3
+//        Albánia 3 2 4
+//        Libanon 4 3 2
