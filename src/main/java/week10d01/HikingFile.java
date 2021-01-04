999215f458e4d23a06628d412ae88d16c2a01815
kondasg <gabor@kondas.hu>
2021-01-04 12:02:37 +0100

    órai feladat

--- src/main/java/week10d01/HikingFile.java
+++ src/main/java/week10d01/HikingFile.java
@@ -7,40 +7,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HikingFile {
-    
+
     public Elevation getElevation(String file) {
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(
                 HikingFile.class.getResourceAsStream(file)))) {
             String line;
             List<Double> elevation = new ArrayList<>();
-            while((line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 String[] data = line.split(",");
                 elevation.add(Double.parseDouble(data[2].trim()));
             }
             double prevElevation = elevation.get(0);
             double plus = 0;
             double minus = 0;
-            for (double item: elevation) {
+            for (double item : elevation) {
                 if (prevElevation < item) {
-                    plus = Double.sum(plus,item-prevElevation);
+                    plus = Double.sum(plus, item - prevElevation);
                 }
                 if (prevElevation > item) {
-                    minus = Double.sum(minus,item-prevElevation);
+                    minus = Double.sum(minus, item - prevElevation);
                 }
                 prevElevation = item;
             }
 
-            return new Elevation(plus,Math.abs(minus));
-        }
-        catch (IOException ioe) {
+            return new Elevation(plus, Math.abs(minus));
+        } catch (IOException ioe) {
             throw new IllegalStateException("Can not read file", ioe);
         }
-
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new HikingFile().getElevation("gps.txt"));
     }
 
 }
