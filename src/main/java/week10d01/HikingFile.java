d9379961023cc0dc3b155aade52e076869155824
kondasg <gabor@kondas.hu>
2021-01-04 10:46:01 +0100

    órai feladat

--- src/main/java/week10d01/HikingFile.java
+++ src/main/java/week10d01/HikingFile.java
@@ -16,7 +16,7 @@ public class HikingFile {
             List<Double> elevation = new ArrayList<>();
             while((line = reader.readLine()) != null) {
                 String[] data = line.split(",");
-                elevation.add(Double.parseDouble(data[2]));
+                elevation.add(Double.parseDouble(data[2].trim()));
             }
             double prevElevation = elevation.get(0);
             double plus = 0;
@@ -44,3 +44,12 @@ public class HikingFile {
     }
 
 }
+
+// Túrázás közben egy GPS eszköz rögzíti a pontokat. Eltárolja a GPS koordinátákat, valamint a magasságot (mindegyik lebegőpontos érték).
+// Ezt egy fájlba tárolja, melynek formátuma:
+//  47.497913, 19.040236, 400
+//  47.497912, 19.040232, 420
+//  47.497916, 19.040236, 410
+// Írj a week10d01.HikingFile osztályba egy getPlusElevation() metódust, mely kap egy InputStream-et, és visszaadja
+// külön az ereszkedések, és külön az emelkedések összegét. Azaz pl. 10,20,15,18 esetén az emelkedés 13, ugyanis (20 - 10) + (18 - 15).
+// A 20 méterről 15 méterre ereszkedést pedig 5.
