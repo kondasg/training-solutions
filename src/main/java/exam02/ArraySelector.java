15c1d440b968d58aa8b09a3106c0329a6ae7ade3
kondasg <gabor@kondas.hu>
2021-01-06 19:15:41 +0100

    Tömbök

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -2,5 +2,18 @@ package exam02;
 
 public class ArraySelector {
 
-
+    public String selectEvens(int[] numbers) {
+        if (numbers.length == 0) return "";
+        StringBuilder result = new StringBuilder();
+        result.append("[");
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 2 == 0) {
+                if (i > 0) {
+                    result.append(", ");
+                }
+                result.append(numbers[i]);
+            }
+        }
+        return result.append("]").toString();
+    }
 }
