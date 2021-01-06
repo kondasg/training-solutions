e5d26d7342b09ca55beb9328a366a897f5cb50f4
kondasg <gabor@kondas.hu>
2021-01-06 18:02:55 +0100

    Fájlkezelés - lottószámok - részmegoldás (az idő lejárt)

--- src/main/java/exam02/LottoReader.java
+++ src/main/java/exam02/LottoReader.java
@@ -1,4 +1,30 @@
 package exam02;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class LottoReader {
+
+    private final int[] numbers = new int[90];
+
+    public LottoReader(InputStream inputStream) {
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(";");
+                //int x = Integer.parseInt(data[0]);
+            }
+
+
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+    }
+
+    public int getNumber(int number) {
+        return numbers[number-1];
+    }
 }
