0d9d4402ae2bf4657ba7169dde55ad63f7925948
kondasg <gabor@kondas.hu>
2021-01-08 18:34:19 +0100

    System.out.print tesztelése

---
+++ src/main/java/week10d05/Sout.java
@@ -0,0 +1,8 @@
+package week10d05;
+
+public class Sout {
+
+    public void sout() {
+        System.out.println("Sout test!");
+    }
+}
---
+++ src/test/java/week10d05/SoutTest.java
@@ -0,0 +1,34 @@
+package week10d05;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SoutTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    void givenSystemOutRedirection_whenInvokePrintln_thenOutputCaptorSuccess() {
+        new Sout().sout();
+
+        assertEquals("Sout test!", outputStreamCaptor.toString().trim());
+    }
+
+
+}
