d62b13be73bbca204120c0eb81ca1fb12f250411
kondasg <gabor@kondas.hu>
2021-01-14 16:12:59 +0100

    +1 metódus

---
+++ src/main/java/week11d04/NameLength.java
@@ -0,0 +1,30 @@
+package week11d04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NameLength {
+
+    public List<Integer> getLengths(List<String> names) {
+        List<Integer> lengthOfNames = new ArrayList<>();
+        for (String name: names) {
+            if (name.startsWith("J")) {
+                int length = names.size();
+                if (!lengthOfNames.contains(length)) {
+                    lengthOfNames.add(length);
+                }
+            }
+        }
+        return lengthOfNames;
+    }
+
+    public static void main(String[] args) {
+        List<String> s = List.of("Joe", "Jack", "Jane", "Jake", "George", "William");
+        System.out.println(new NameLength().getLengths(s));
+    }
+}
+
+// Mai senior feladat:
+// Adott egy nevek listája. Add vissza egy listában, hogy milyen hosszúságú J karakterrel kezdődő nevek szerepelnek a listában!
+// Minden szám csak egyszer szerepeljen! A NameLength osztály getLengths(List<String>) metódusába dolgozz!
+// Ha a bemenet pl. List.of("Joe", "Jack", "Jane", "Jake", "George", "William"), a visszatérési érték [3, 4] legyen!
