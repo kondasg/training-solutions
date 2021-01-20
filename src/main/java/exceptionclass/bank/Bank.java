c292bedc781e67d2528af937c215cbde7f837a5d
kondasg <gabor@kondas.hu>
2021-01-20 11:16:05 +0100

    Bank példa

---
+++ src/main/java/exceptionclass/bank/Bank.java
@@ -0,0 +1,35 @@
+package exceptionclass.bank;
+
+import java.util.List;
+
+public class Bank {
+
+    List<Account> accounts;
+
+    public Bank(List<Account> accounts) {
+        if (accounts == null) {
+            throw new IllegalArgumentException();
+        }
+        this.accounts = accounts;
+    }
+
+    public void payment(String accountNumber, double amount) {
+        for (Account account: accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                account.subtract(amount);
+                return;
+            }
+        }
+        throw new InvalidBankOperationException(ErrorCode.INVALID_ACCOUNTNUMBER);
+    }
+
+    public void deposit(String accountNumber, double amount) {
+        for (Account account: accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                account.deposit(amount);
+                return;
+            }
+        }
+        throw new InvalidBankOperationException(ErrorCode.INVALID_ACCOUNTNUMBER);
+    }
+}
