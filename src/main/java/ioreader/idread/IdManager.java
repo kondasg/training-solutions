00735bf2539a7f2fb49c2aab9ee2415ffcf3f6cb
kondasg <gabor@kondas.hu>
2021-01-20 23:55:07 +0100

    Személyi igazolvány számok

---
+++ src/main/java/ioreader/idread/IdManager.java
@@ -0,0 +1,30 @@
+package ioreader.idread;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IdManager {
+
+    private final List<String> ids = new ArrayList<>();
+
+    public List<String> getIds() {
+        return ids;
+    }
+
+    public void readIdsFromFile(String filename) {
+        Path file = Path.of(filename);
+
+        try (BufferedReader reader = Files.newBufferedReader(file)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                ids.add(line);
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Can't read file", e);
+        }
+    }
+}
