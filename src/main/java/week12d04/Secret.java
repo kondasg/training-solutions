6c7ddb1fa087661520a7ebf33f9b0c140dedad7f
kondasg <gabor@kondas.hu>
2021-01-21 12:20:40 +0100

    Junior/Mid feladat

---
+++ src/main/java/week12d04/Secret.java
@@ -0,0 +1,26 @@
+package week12d04;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Secret {
+
+    public void readFile(String fileName) {
+        Path file = Path.of(fileName);
+
+        try {
+            byte[] bytes = Files.readAllBytes(file);
+            for (byte item: bytes) {
+                System.out.print((char)(item + 10));
+            }
+        }
+        catch (IOException e) {
+            throw new IllegalStateException("Can't read file", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Secret().readFile("secret.dat");
+    }
+}
