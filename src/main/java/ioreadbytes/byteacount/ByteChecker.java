27b154338c2e42610a848fc3eb35522f955792c1
kondasg <gabor@kondas.hu>
2021-01-23 23:35:29 +0100

    "A" betűk

---
+++ src/main/java/ioreadbytes/byteacount/ByteChecker.java
@@ -0,0 +1,30 @@
+package ioreadbytes.byteacount;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ByteChecker {
+
+    public int readBytesAndFindAs(String dataFile) {
+        try (InputStream inputStream = new BufferedInputStream(Files.newInputStream(Path.of(dataFile)))) {
+            byte[] bytes = new byte[1000];
+            int size;
+            int sum = 0;
+            while ((size = inputStream.read(bytes)) > 0) {
+                for (byte item: bytes) {
+                    if (item == 'a') {
+                        sum++;
+                    }
+                }
+                bytes = new byte[1000];
+            }
+            return sum;
+        }
+        catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+    }
+}
