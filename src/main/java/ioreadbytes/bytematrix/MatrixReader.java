44e8c0cd0d1da78f7d8b7fb23c22a312b6fa0994
kondasg <gabor@kondas.hu>
2021-01-24 00:55:48 +0100

    Mátrix

---
+++ src/main/java/ioreadbytes/bytematrix/MatrixReader.java
@@ -0,0 +1,48 @@
+package ioreadbytes.bytematrix;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatrixReader {
+    
+    private final List<byte[]> myMatrix = new ArrayList<>();
+
+    public List<byte[]> getMyMatrix() {
+        return myMatrix;
+    }
+
+    public void readBytesAndCreateMatrix(String dataFile) {
+        try (InputStream inputStream = new BufferedInputStream(Files.newInputStream(Path.of(dataFile)))) {
+            byte[] bytes = new byte[1000];
+            int size;
+            while ((size = inputStream.read(bytes)) > 0) {
+                myMatrix.add(bytes);
+            }
+        }
+        catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+    }
+
+    public int numberOfColumnsWhereMoreZeros() {
+        int[] numberOfOne = new int[1000];
+        for (byte[] matrix : myMatrix) {
+            for (int j = 0; j < matrix.length; j++) {
+                int x = Integer.parseInt(String.valueOf((char) matrix[j]));
+                numberOfOne[j] += x;
+            }
+        }
+        int col = 0;
+        for (int item: numberOfOne) {
+            if (item < 3) {
+                col++;
+            }
+        }
+        return col;
+    }
+}
