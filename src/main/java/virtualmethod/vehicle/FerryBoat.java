d57767a6edeb7926ada1ca8e5d5e43e0025ed061
kondasg <gabor@kondas.hu>
2021-01-28 22:12:57 +0100

    Gyakorlat 2 - FerryBoat

---
+++ src/main/java/virtualmethod/vehicle/FerryBoat.java
@@ -0,0 +1,33 @@
+package virtualmethod.vehicle;
+
+public class FerryBoat extends Vehicle {
+
+    public final int MAX_CARRY_WEIGHT = 2000;
+    Car car = null;
+
+    public FerryBoat(int vehicleWeight) {
+        super(vehicleWeight);
+    }
+
+    public boolean canCarry(Car car) {
+        return car.getGrossLoad() < MAX_CARRY_WEIGHT;
+    }
+
+    public boolean load(Car car) {
+        if (canCarry(car)) {
+            this.car = car;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getGrossLoad() {
+        return super.getGrossLoad() + car.getGrossLoad();
+    }
+
+    @Override
+    public String toString() {
+        return "FerryBoat{car=" + car + "}";
+    }
+}
