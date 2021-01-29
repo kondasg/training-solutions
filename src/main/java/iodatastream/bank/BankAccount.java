7f8968402dbb2148c124304c07da4c2cb30c554a
kondasg <gabor@kondas.hu>
2021-01-29 19:18:25 +0100

    Adatok írása OutputStreamre, és olvasás - BankAccount

--- src/main/java/iodatastream/bank/BankAccount.java
+++ src/main/java/iodatastream/bank/BankAccount.java
@@ -2,8 +2,25 @@ package iodatastream.bank;
 
 public class BankAccount {
 
-    private String
-    private String
-    private double
+    private final String accountNumber;
+    private final String owner;
+    private final double balance;
 
+    public BankAccount(String accountNumber, String owner, double balance) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
--- src/main/java/iodatastream/bank/BankAccountManager.java
+++ src/main/java/iodatastream/bank/BankAccountManager.java
@@ -1,6 +1,28 @@
 package iodatastream.bank;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class BankAccountManager {
 
+    public void saveAccount(BankAccount account, Path path) {
+        Path file = path.resolve(account.getAccountNumber() + ".dat");
+        try (DataOutputStream dataOutputStream =
+                     new DataOutputStream(new BufferedOutputStream(Files.newOutputStream(file)))) {
+            dataOutputStream.writeUTF(account.getAccountNumber());
+            dataOutputStream.writeUTF(account.getOwner());
+            dataOutputStream.writeDouble(account.getBalance());
+        } catch (IOException e) {
+            throw new IllegalStateException("Can't write file", e);
+        }
+    }
 
+    public BankAccount loadAccount(InputStream is) {
+        try (DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(is))) {
+            return new BankAccount(dataInputStream.readUTF(), dataInputStream.readUTF(), dataInputStream.readDouble());
+        } catch (IOException e) {
+            throw new IllegalStateException("Can't read file", e);
+        }
+    }
 }
