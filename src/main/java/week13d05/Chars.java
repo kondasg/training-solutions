9a2a4ac5b5590eb65a8ad96a182d50e28c9e3bb3
kondasg <gabor@kondas.hu>
2021-01-29 15:52:16 +0100

    Junior/Mid-level feladat week13d05

---
+++ src/main/java/week13d05/Chars.java
@@ -0,0 +1,22 @@
+package week13d05;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Chars {
+
+    public int countChar(String word) {
+        char[] chars = word.toCharArray();
+        Set<String> result  = new HashSet<>();
+        for (char c: chars ) {
+            if (c >= 'a' && c <= 'z') {
+                result.add(c+"");
+            }
+        }
+        return result.size();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Chars().countChar("abcdeabcde"));
+    }
+}
