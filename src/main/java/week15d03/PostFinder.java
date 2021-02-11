84d9160148da1075d37709674f0e3fb925cc9804
kondasg <gabor@kondas.hu>
2021-02-11 12:27:36 +0100

    Junior/Mid-level feladat week15d03 + hibakezelés + teszt

--- src/main/java/week15d03/PostFinder.java
+++ src/main/java/week15d03/PostFinder.java
@@ -17,16 +17,47 @@ public class PostFinder {
     }
 
     public List<Post> findPostsFor(String user) {
+        if (user == null || user.isBlank()) {
+            throw new IllegalArgumentException("user is empty or null!");
+        }
         List<Post> result = new ArrayList<>();
-        for (Post post: posts) {
-            if ( user.equals(post.getOwner())
+        for (Post post : posts) {
+            if (isNullPostsArguments(post)) {
+                throw new IllegalArgumentException("Post argument is null");
+            }
+            if (user.equals(post.getOwner())
                     && LocalDate.now().isAfter(post.getPublishedAt())
                     && !post.getContent().isEmpty()
                     && !post.getTitle().isEmpty()) {
                 result.add(post);
             }
         }
-
         return result;
     }
+
+    private boolean isNullPostsArguments(Post post) {
+        //Objects.requireNonNull
+        return (post.getContent() == null
+                || post.getTitle() == null
+                || post.getOwner() == null
+                || post.getPublishedAt() == null);
+    }
+
 }
+
+// Mai junior/medior feladat:
+// Adott az alábbi osztály:
+//
+// java
+//  public class Post {
+//    private String title;
+//    private LocalDate publishedAt;
+//    private String content;
+//    private String owner;
+//  }
+//
+// Implementáljunk a PostFinder osztályban egy public List<Post> findPostsFor(String user) metódust az alábbi módon:
+//        - Csak olyan Post-ot adjunk vissza, amelynek az owner-e megegyezik a megadott user-rel
+//        - A Post publishedAt dátuma korábbi kell, hogy legyen, mint a LocalDate.now()
+//        - A content és a title nem lehet üres
+// Tároljunk egy List<Post> objektumot a PostFinder osztályban, melyet konstruktor paraméterként kap meg és ezt használjuk!
--- src/test/java/week15d03/PostFinderTest.java
+++ src/test/java/week15d03/PostFinderTest.java
@@ -1,6 +1,7 @@
 package week15d03;
 
 import org.junit.jupiter.api.Test;
+import stringscanner.StringScanner;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -9,17 +10,48 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PostFinderTest {
 
+    List<Post> l = List.of(
+            new Post("Title1", LocalDate.of(2020,1,1), "Content1",  "Owner1"),
+            new Post("Title2", LocalDate.of(2021,5,1), "Content2",  "Owner1"),
+            new Post("Title3", LocalDate.of(2020,1,1), "Content3",  "Owner1"),
+            new Post("Title4", LocalDate.of(2020,1,1), "Content4",  "Owner2")
+    );
+    PostFinder postFinder = new PostFinder(l);
+
+    @Test
+    void userIsNull() throws IllegalArgumentException {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> postFinder.findPostsFor(null));
+        assertEquals("user is empty or null!", ex.getMessage());
+
+        ex = assertThrows(IllegalArgumentException.class, () -> postFinder.findPostsFor(""));
+        assertEquals("user is empty or null!", ex.getMessage());
+    }
+
+    @Test
+    void postIsNull() throws IllegalArgumentException {
+        l = List.of(new Post(null, LocalDate.of(2020,1,1), "Content4",  "Owner1"));
+        postFinder = new PostFinder(l);
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> postFinder.findPostsFor("Owner1"));
+        assertEquals("Post argument is null", ex.getMessage());
+
+        l = List.of(new Post(null, null, "Content4",  "Owner1"));
+        postFinder = new PostFinder(l);
+        ex = assertThrows(IllegalArgumentException.class, () -> postFinder.findPostsFor("Owner1"));
+        assertEquals("Post argument is null", ex.getMessage());
+
+        l = List.of(new Post(null, null, null,  "Owner1"));
+        postFinder = new PostFinder(l);
+        ex = assertThrows(IllegalArgumentException.class, () -> postFinder.findPostsFor("Owner1"));
+        assertEquals("Post argument is null", ex.getMessage());
+
+        l = List.of(new Post(null, null, null,  null));
+        postFinder = new PostFinder(l);
+        ex = assertThrows(IllegalArgumentException.class, () -> postFinder.findPostsFor("Owner1"));
+        assertEquals("Post argument is null", ex.getMessage());
+    }
+
     @Test
     void findPostsFor() {
-        List<Post> l = List.of(
-                new Post("Title1", LocalDate.of(2020,1,1), "Content1",  "Owner1"),
-                new Post("Title2", LocalDate.of(2021,5,1), "Content2",  "Owner1"),
-                new Post("Title3", LocalDate.of(2020,1,1), "Content3",  "Owner1"),
-                new Post("Title4", LocalDate.of(2020,1,1), "Content4",  "Owner2")
-        );
-        PostFinder postFinder = new PostFinder(l);
-
         assertEquals(2, postFinder.findPostsFor("Owner1").size());
-
     }
 }
