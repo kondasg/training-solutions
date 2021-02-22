58ac3140dbfe15fc909cac440e9e60c748c604d6
kondasg <gabor@kondas.hu>
2021-02-22 21:07:28 +0100

    exam03retake01 - CDV

--- src/main/java/exam03retake01/CdvCheck.java
+++ src/main/java/exam03retake01/CdvCheck.java
@@ -2,4 +2,18 @@ package exam03retake01;
 
 public class CdvCheck {
 
+    public boolean check(String s) {
+        if (s.length() != 10) {
+            throw new IllegalArgumentException();
+        }
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            try {
+                sum += (i + 1) * Integer.parseInt(s.substring(i, i + 1));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException();
+            }
+        }
+        return sum % 11 == Integer.parseInt(s.substring(9, 10));
+    }
 }
