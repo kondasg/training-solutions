cbcc258b334188257a75e6f593eee8fd265ec3a4
kondasg <gabor@kondas.hu>
2021-02-22 21:17:18 +0100

    exam03retake01 - Bagolyszámlálás

--- src/main/java/exam03retake01/OwlCounter.java
+++ src/main/java/exam03retake01/OwlCounter.java
@@ -1,5 +1,27 @@
 package exam03retake01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class OwlCounter {
 
+    private final Map<String, Integer> owls = new HashMap<>();
+
+    public void readFromFile(BufferedReader reader) {
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] splittedLine = line.split("=");
+                owls.put(splittedLine[0], Integer.parseInt(splittedLine[1]));
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Read error", e);
+        }
+    }
+
+    public int getNumberOfOwls(String county) {
+        return owls.get(county);
+    }
 }
