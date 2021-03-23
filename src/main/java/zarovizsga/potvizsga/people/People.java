7808f710d1ab35098582f25a37af6edba94cf65e
kondasg <gabor@kondas.hu>
2021-03-23 11:51:33 +0100

    Záróvizsga pótvizsga - Emberek done

--- src/main/java/zarovizsga/potvizsga/people/People.java
+++ src/main/java/zarovizsga/potvizsga/people/People.java
@@ -1,5 +1,25 @@
 package zarovizsga.potvizsga.people;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class People {
 
+    public int getNumberOfMales(String path) {
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(path))) {
+            int countMale = 0;
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] splittedLine = line.split(",");
+                if (splittedLine[4].equals("Male")) {
+                    countMale++;
+                }
+            }
+            return countMale;
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+    }
 }
