2e0c280f54cbffb79564b22e78cf4019ff87cfb7
kondasg <gabor@kondas.hu>
2021-03-23 11:43:05 +0100

    Záróvizsga pótvizsga - Számjegyek done

--- src/main/java/zarovizsga/potvizsga/words/Words.java
+++ src/main/java/zarovizsga/potvizsga/words/Words.java
@@ -1,4 +1,15 @@
 package zarovizsga.potvizsga.words;
 
 public class Words {
+
+    public boolean hasMoreDigits(String s) {
+        int countDigits = 0;
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                countDigits++;
+            }
+        }
+        return s.length() - countDigits < countDigits;
+    }
+
 }
