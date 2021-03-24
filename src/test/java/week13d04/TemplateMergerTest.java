df584a452bf6e1e93b06359a13b618617ff49ca6
kondasg <gabor@kondas.hu>
2021-03-24 22:59:25 +0100

    Junior week13d04 - read file main\resources

--- src/test/java/week13d04/TemplateMergerTest.java
+++ src/test/java/week13d04/TemplateMergerTest.java
@@ -15,6 +15,6 @@ class TemplateMergerTest {
         List<Employee> employees = List.of(new Employee("John Doe", 1980), new Employee("Jack Doe", 1990));
         String result = "Az alkalmazott neve: John Doe, születési éve: 1980\n" +
                 "Az alkalmazott neve: Jack Doe, születési éve: 1990\n";
-        assertEquals(result, templateMerger.merge(Path.of("temp.txt"), employees));
+        assertEquals(result, templateMerger.merge(Path.of("src/main/resources/week13d04/temp.txt"), employees));
     }
 }
