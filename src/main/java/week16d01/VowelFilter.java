f513407ae08ee9af843d76f29d872a87190f58ad
kondasg <gabor@kondas.hu>
2021-03-24 22:44:48 +0100

    VowelFilter - read file classpath + hibajavítás

--- src/main/java/week16d01/VowelFilter.java
+++ src/main/java/week16d01/VowelFilter.java
@@ -2,8 +2,7 @@ package week16d01;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,7 +35,7 @@ public class VowelFilter {
     private void newWord(String word) {
         StringBuilder newWord = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
-            if (!VOWELS.contains(word.charAt(i))) {
+            if (!VOWELS.contains(word.toLowerCase().charAt(i))) {
                 newWord.append(word.charAt(i));
             }
         }
@@ -44,7 +43,8 @@ public class VowelFilter {
     }
 
     public static void main(String[] args) {
-        try (BufferedReader br = Files.newBufferedReader(Path.of("VowelFilter.txt"))) {
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(VowelFilter.class.getResourceAsStream("VowelFilter.txt")))) {
             System.out.println(new VowelFilter().filterFowels(br));
         } catch (IOException e) {
             throw new IllegalStateException("Can't read file", e);
