060f6d4f7c86303a27f8bce1a4dc647efebdc551
kondasg <gabor@kondas.hu>
2021-03-30 18:08:13 +0200

    Pótpótvizsga - Unique finder done

--- src/main/java/zarovizsga/potpotvizsga/uniquefinder/UniqueFinder.java
+++ src/main/java/zarovizsga/potpotvizsga/uniquefinder/UniqueFinder.java
@@ -1,5 +1,17 @@
 package zarovizsga.potpotvizsga.uniquefinder;
 
+import java.util.*;
+
 public class UniqueFinder {
 
+    public List<Character> uniqueChars(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException("s is null");
+        }
+        Set<Character> result = new LinkedHashSet<>();
+        for (Character c : s.toCharArray()) {
+            result.add(c);
+        }
+        return new ArrayList<>(result);
+    }
 }
--- src/test/java/zarovizsga/potpotvizsga/uniquefinder/UniqueFinderTest.java
+++ src/test/java/zarovizsga/potpotvizsga/uniquefinder/UniqueFinderTest.java
@@ -1,4 +1,4 @@
-package hu.nive.ujratervezes.zarovizsga.uniquefinder;
+package zarovizsga.potpotvizsga.uniquefinder;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
