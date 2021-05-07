cd29b30cd59558d4fa161b97a33d48de5bbf09ce
kondasg <gabor@kondas.hu>
2021-05-07 23:07:32 +0200

    cizellálás

--- src/main/java/activitytracker/ActivityTrackerDao.java
+++ src/main/java/activitytracker/ActivityTrackerDao.java
@@ -14,30 +14,31 @@ public class ActivityTrackerDao {
     }
 
     public void crateActivities(List<Activity> activities) {
-        try (
-                Connection conn = dataSource.getConnection();
-                PreparedStatement stmt =
-                        conn.prepareStatement("INSERT INTO activities (start_time, activity_desc, activity_type) values (?, ?, ?)")) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt =
+                     conn.prepareStatement(
+                             "INSERT INTO `activities` (`start_time`, `activity_desc`, `activity_type`) " +
+                                     "VALUES (?, ?, ?)")) {
             for (Activity activity : activities) {
                 stmt.setTimestamp(1, Timestamp.valueOf(activity.getStartTime()));
                 stmt.setString(2, activity.getDesc());
                 stmt.setString(3, activity.getType().toString());
                 stmt.executeUpdate();
             }
-        } catch (
-                SQLException se) {
-            throw new IllegalStateException("Can't insert", se);
+        } catch (SQLException se) {
+            throw new IllegalStateException("Can't INSERT", se);
         }
     }
 
     public Activity selectActivityById(int id) {
         try (Connection conn = dataSource.getConnection();
              PreparedStatement stmt =
-                     conn.prepareStatement("SELECT * FROM activities WHERE id = ?")) {
+                     conn.prepareStatement(
+                             "SELECT `id`, `start_time`, `activity_desc`, `activity_type` " +
+                                     "FROM `activities` WHERE `id` = ?")) {
             stmt.setInt(1, id);
             return getActivity(stmt);
-        }
-        catch (SQLException se) {
+        } catch (SQLException se) {
             throw new IllegalStateException("Can't SELECT'", se);
         }
     }
@@ -45,32 +46,35 @@ public class ActivityTrackerDao {
     private Activity getActivity(PreparedStatement stmt) {
         try (ResultSet rs = stmt.executeQuery()) {
             if (rs.next()) {
-                return new Activity(rs.getInt(1), rs.getTimestamp(2).toLocalDateTime(),
-                        rs.getString(3), ActivityType.valueOf(rs.getString(4)));
+                return new Activity(
+                        rs.getInt("id"),
+                        rs.getTimestamp("start_time").toLocalDateTime(),
+                        rs.getString("activity_desc"),
+                        ActivityType.valueOf(rs.getString("activity_type")));
             }
-            throw new IllegalStateException("Can't find ID");
-        } catch (SQLException sqle) {
-            throw new IllegalArgumentException("Error by SELECT", sqle);
+            throw new IllegalArgumentException("Can't find ID");
+        } catch (SQLException se) {
+            throw new IllegalArgumentException("Error by SELECT", se);
         }
     }
 
     public List<Activity> selectActivities() {
         try (Connection conn = dataSource.getConnection();
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT * FROM activities")) {
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(
+                     "SELECT `id`, `start_time`, `activity_desc`, `activity_type` FROM `activities`")) {
             List<Activity> activities = new ArrayList<>();
             while (rs.next()) {
                 Activity activity = new Activity(
-                        rs.getInt(1),
-                        rs.getTimestamp(2).toLocalDateTime(),
-                        rs.getString(3),
-                        ActivityType.valueOf(rs.getString(4))
+                        rs.getInt("id"),
+                        rs.getTimestamp("start_time").toLocalDateTime(),
+                        rs.getString("activity_desc"),
+                        ActivityType.valueOf(rs.getString("activity_type"))
                 );
                 activities.add(activity);
             }
             return activities;
-        }
-        catch (SQLException se) {
+        } catch (SQLException se) {
             throw new IllegalStateException("Can't SELECT'", se);
         }
     }
